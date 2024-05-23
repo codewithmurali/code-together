3b0e3de5af55759b22b6a1385f99699345b5f996
MURALIKUMAR J <muralikumar.admire@gmail.com>
2024-05-23 21:35:50 +0530

    Displayed the src into the Iframe

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import "./App.css";
 import EditorContainer from "./components/EditorContainer";
 
@@ -6,17 +6,35 @@ function App() {
   const [html, setHtml]  = useState("");
   const [css, setCss] = useState("");
   const [js, setJs] = useState("");
+  const [srcDoc, setSrcDoc]  = useState("");
+  useEffect(()=>{
+    setSrcDoc(
+      `<html>
+        <head></head>
+        <body>
+          ${html}
+        </body>
+        <style>${css}</style>
+        <script>${js}</script>
+      </html>`
+    )
+  }, [html, css, js])
 
   return (
-    <>
-      <div className="panel top-panel"></div>
+    <div>
+      <div className="nav">
+        <h3>Code-together</h3>
+      </div>
       <div className="panel">
         <EditorContainer title={"HTML"} language={"html"} value = {html} onChange={setHtml} />
         <EditorContainer title={"CSS"} language={"css"} value={css} onChange={setCss}/>
         <EditorContainer title={"JavaScript"} language={"javascript"} value={js} onChange={setJs} />
       </div>
+      <div className="display-container">
+        <iframe src={srcDoc}></iframe>
+      </div>
                        
-    </>
+    </div>
   );
 }
 
--- src/components/EditorContainer.jsx
+++ src/components/EditorContainer.jsx
@@ -1,29 +1,28 @@
-import React, { useReducer } from "react";
+import React, { useState } from "react";
 import Editor from "@monaco-editor/react";
-import {LaunchIcon , TransitEnterexitIcon} from '@mui/icons-material/Launch';
+import {Launch , TransitEnterexit} from '@mui/icons-material';
 
 const EditorContainer = ({ language, title, value, onChange }) => {
   const [open,  setOpen ] = useState(true);
-
-
+ 
   function handleChange(editor, data, value ){
     onChange(value)
   }
 
    return (
-    <div className="editor-container">
+    <div className={`editor-container `}>
       <div className="editor-title">
         <h3>{title}</h3>
         <button onClick = {()=> setOpen((prev)=> !prev)}>
-          {open ?<LaunchIcon/> : <TransitEnterexitIcon/>}
+          {open ?<Launch/> : <TransitEnterexit/>}
         </button>
       </div>
       <Editor
-      onChange={handleChange}
-  value={value}
-        className="code-editor"
+        onChange={handleChange}
+        value={value}
+        className={`code-editor ${open && "collapsed"}`}
         width= "32vw"
-        height="50vh"
+        height="40vh"
         defaultLanguage={language}
         theme="vs-dark"
       />
