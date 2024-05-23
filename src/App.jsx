3a68bb7f8bafea0020f7bd5f7896220b6006f0ee
MURALIKUMAR J <muralikumar.admire@gmail.com>
2024-05-24 00:04:33 +0530

    Making some changes in the layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState,useEffect } from "react";
-import "./App.css";
+import ViewWeekSharpIcon from '@mui/icons-material/ViewWeekSharp';
+import TableRowsSharpIcon from '@mui/icons-material/TableRowsSharp';
 import EditorContainer from "./components/EditorContainer";
 
 function App() {
@@ -7,34 +8,47 @@ function App() {
   const [css, setCss] = useState("");
   const [js, setJs] = useState("");
   const [srcDoc, setSrcDoc]  = useState("");
+  const [layoutView , setLayoutView] = useState(true);
   useEffect(()=>{
-    setSrcDoc(
-      `<html>
-        <head></head>
-        <body>
-          ${html}
-        </body>
-        <style>${css}</style>
-        <script>${js}</script>
-      </html>`
-    )
-  }, [html, css, js])
+
+    const timeOut = setTimeout(()=>{
+      setSrcDoc(
+        `<html>
+          <head></head>
+          <body>
+            ${html}
+          </body>
+          <style>${css}</style>
+          <script>${js}</script>
+        </html>`
+      );
+    }, 250)
+ return ()=>{
+  clearTimeout(timeOut)
+ }
+}, [html, css, js])
 
   return (
     <div>
       <div className="nav">
         <h3>Code-together</h3>
+        <button onClick={() => { setLayoutView((prev) => !prev) }}>
+          {layoutView ? <ViewWeekSharpIcon /> : <TableRowsSharpIcon />}
+        </button>
       </div>
-      <div className="panel">
-        <EditorContainer title={"HTML"} language={"html"} value = {html} onChange={setHtml} />
-        <EditorContainer title={"CSS"} language={"css"} value={css} onChange={setCss}/>
-        <EditorContainer title={"JavaScript"} language={"javascript"} value={js} onChange={setJs} />
+      <div className={`layout ${layoutView ? "layout-column" : "layout-row"}`}>
+        <div className="panel">
+          <EditorContainer title={"HTML"} language={"html"} value={html} onChange={setHtml} />
+          <EditorContainer title={"CSS"} language={"css"} value={css} onChange={setCss} />
+          <EditorContainer title={"JavaScript"} language={"javascript"} value={js} onChange={setJs} />
+        </div>
+        <div className="display-container">
+          <iframe src={srcDoc}></iframe>
+        </div>
       </div>
-      <div className="display-container">
-        <iframe src={srcDoc}></iframe>
-      </div>
-                       
     </div>
+
+  
   );
 }
 
--- src/components/EditorContainer.jsx
+++ src/components/EditorContainer.jsx
@@ -3,7 +3,7 @@ import Editor from "@monaco-editor/react";
 import {Launch , TransitEnterexit} from '@mui/icons-material';
 
 const EditorContainer = ({ language, title, value, onChange }) => {
-  const [open,  setOpen ] = useState(true);
+  const [open,  setOpen ] = useState(false);
  
   function handleChange(editor, data, value ){
     onChange(value)
@@ -11,21 +11,23 @@ const EditorContainer = ({ language, title, value, onChange }) => {
 
    return (
     <div className={`editor-container `}>
-      <div className="editor-title">
-        <h3>{title}</h3>
+      <div className="editor-title" >
+        <h4>{title}</h4>
         <button onClick = {()=> setOpen((prev)=> !prev)}>
           {open ?<Launch/> : <TransitEnterexit/>}
         </button>
       </div>
+      <div className={`code-editor ${open && "collapsed"}`}>
       <Editor
         onChange={handleChange}
         value={value}
-        className={`code-editor ${open && "collapsed"}`}
         width= "32vw"
         height="40vh"
         defaultLanguage={language}
         theme="vs-dark"
       />
+      </div>
+      
     </div>
   );
 };
