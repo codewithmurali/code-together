394f7cd95236471b655d5edca9595bc51c95ac67
MURALIKUMAR J <muralikumar.admire@gmail.com>
2024-06-03 23:42:31 +0530

    After implementation of Web Sockets

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,54 @@
-import { useState, useEffect } from "react";
-import ViewWeekSharpIcon from '@mui/icons-material/ViewWeekSharp';
-import TableRowsSharpIcon from '@mui/icons-material/TableRowsSharp';
+import { useState, useEffect, useRef } from "react";
+import ViewWeekSharpIcon from "@mui/icons-material/ViewWeekSharp";
+import TableRowsSharpIcon from "@mui/icons-material/TableRowsSharp";
 import EditorContainer from "./components/EditorContainer";
+import io from "socket.io-client";
+import "./App.css";
+
+const socket = io("http://localhost:4000");
 
 function App() {
-  const [html, setHtml] = useState(""); 
-  const [css, setCss] = useState(""); 
-  const [js, setJs] = useState("");   
-  const [srcDoc, setSrcDoc] = useState("");
-  const [layoutView, setLayoutView] = useState(true); 
+  const [html, setHtml] = useState("");
+  const [css, setCss] = useState("");
+  const [js, setJs] = useState("");
+  const [layoutView, setLayoutView] = useState(true);
+  const iframeRef = useRef(null);
+
+  useEffect(() => {
+    socket.on("update", (data) => {
+      setHtml(data.html);
+      setCss(data.css);
+      setJs(data.js);
+    });
+
+    return () => {
+      socket.off("update");
+    };
+  }, []);
 
   useEffect(() => {
     const timeout = setTimeout(() => {
-      setSrcDoc(`
+      const doc = `
         <!DOCTYPE html>
         <html>
           <head> 
-          <style>${css}</style>
+            <style>${css}</style>
           </head>
           <body>
             ${html}
             <script>${js}</script>
           </body>
         </html>
-      `);
+      `;
+
+      if (iframeRef.current) {
+        const iframeDoc = iframeRef.current.contentDocument;
+        iframeDoc.open();
+        iframeDoc.write(doc);
+        iframeDoc.close();
+      }
+
+      socket.emit("codeChange", { html, css, js });
     }, 250);
 
     return () => clearTimeout(timeout);
@@ -39,27 +64,31 @@ function App() {
       </div>
       <div className={`layout ${layoutView ? "layout-column" : "layout-row"}`}>
         <div className="panel">
-          <EditorContainer 
-            title={"HTML"} 
-            language={"html"} 
-            value={html} 
-            onChange={(value) => setHtml(value)} 
+          <EditorContainer
+            title={"HTML"}
+            language={"html"}
+            value={html}
+            onChange={(value) => setHtml(value)}
           />
-          <EditorContainer 
-            title={"CSS"} 
-            language={"css"} 
-            value={css} 
-            onChange={(value) => setCss(value)} 
+          <EditorContainer
+            title={"CSS"}
+            language={"css"}
+            value={css}
+            onChange={(value) => setCss(value)}
           />
-          <EditorContainer 
-            title={"JavaScript"} 
-            language={"javascript"} 
-            value={js} 
-            onChange={(value) => setJs(value)} 
+          <EditorContainer
+            title={"JavaScript"}
+            language={"javascript"}
+            value={js}
+            onChange={(value) => setJs(value)}
           />
         </div>
         <div className="display-container">
-          <iframe srcDoc={srcDoc}></iframe>
+          <iframe
+            ref={iframeRef}
+            title="output"
+            onLoad={() => console.log("iframe loaded")}
+          ></iframe>
         </div>
       </div>
     </div>
--- server/index.js
+++ server/index.js
@@ -1,1 +1,36 @@
-const express = require("express");
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"]
+  }
+});
+
+let codeData = { html: '', css: '', js: '' };
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  socket.emit('update', codeData);
+
+  socket.on('codeChange', (data) => {
+    codeData = data;
+    socket.broadcast.emit('update', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+server.listen(4000, () => {
+  console.log('Server is running on port 4000');
+});
